a59e8c6550c2a0b85d5b170978873a846f299ec5
Ronak <ronaksaluja79@gmail.com>
2024-07-21 19:30:48 +0530

    project setup

---
+++ web/themes/pal_cloth/js/pal-cloth.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Pal Cloth behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.palCloth = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
---
+++ web/themes/pal_cloth/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
